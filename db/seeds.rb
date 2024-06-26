1605215154e0238171a983c8a1cd43f3c2c5e03a
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-06-26 12:54:33 +0200

    flat controllers, layouts navbar, simple form
    Signed-off-by: Joaquin Soto <joaquinsotorizzi@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# ADJECTIVES = ["Light", "Enormous", "Cozy", "Well located", "Comfortable"]
+
+10.times do
+  Flat.create!(
+    name: "#{Faker::Emotion.adjective} Flat #{Faker::Address.city}",
+    address: Faker::Address.street_name,
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+    number_of_guests: rand(1..10)
+  )
+end
